23326ed4680db4f633cf2f378420faf6b08e0e85
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-11-04 17:28:03 -0800

    started working on main method

--- HashTable/Main.cpp
+++ HashTable/Main.cpp
@@ -5,5 +5,31 @@ using namespace std;
 
 int main()
 {
-  
+  cout << "Welcome to the cooler version of StudentList commands: ADD, PRINT, DELETE, QUIT" << endl;
+
+  char input[100];
+  bool quit = false;
+
+  while (!quit) {
+
+    cin.get(input, 100);
+    cin.get();
+
+    if (strcmp(input, "ADD") == 0 || strcmp(input, "add") == 0) {
+
+    }
+    else if (strcmp(input, "PRINT") == 0 || strcmp(input, "print") == 0) {
+      
+    }
+    else if (strcmp(input, "DELETE") == 0 || strcmp(input, "delete") == 0) {
+
+    }
+    else if (strcmp(input, "QUIT") == 0 || strcmp(input, "quit") == 0) {
+      quit = true;
+    }
+    else {
+      cout << "Invalid command" << endl;
+    }
+
+  }
 }

bd5077e631b61292f33142e10307d50d910aee52
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-11-23 17:01:43 -0800

    started build function

--- Heap/Main.cpp
+++ Heap/Main.cpp
@@ -3,6 +3,36 @@
 
 using namespace std;
 
+int* BUILD(int* vals);
+
 int main() {
-  cout << "PLACEHOLDER" << endl;
+
+  int* heap = new int[6];
+
+  cout << "Welcome heap lets start with some values to add to the heap" << endl;
+
+  for (int i = 1; i < 6; i++) {
+    cin >> heap[i];
+  }
+
+  heap = BUILD(heap);
+  cout << heap[1] << endl;
+}
+
+int* BUILD(int* vals) {
+  int* newHeap = new int[6];
+
+  int highval = -1000;
+  
+  for (int i = 1; i < 6; i++) {
+    if (vals[i] > highval) {
+      highval = vals[i];
+    }
+  }
+
+  newHeap[1] = highval;
+
+  delete vals;
+  return newHeap;
+  
 }
